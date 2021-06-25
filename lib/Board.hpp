d501a0300466c5519e122e65c2770337368cebe6
Corentin Jabot <corentinjabot@gmail.com>
2021-06-25 11:06:50 +0200

    Implement ghost movement and scatter behavior
    Each ghost is trying to reach a point beyond the
    corner of the board, which make them run in circle

--- lib/Board.hpp
+++ lib/Board.hpp
@@ -18,13 +18,17 @@ public:
     nothing = 2,
     door = 3,
     power_pellet = 4,
-    pen_door = 5,
+    pen = 5,
   };
 
   Board();
 
   [[nodiscard]] bool isWalkableForPacMan(Position point, float d, Direction direction) const;
   [[nodiscard]] bool isWalkableForGhost(Position point, float d, Direction direction) const;
+
+  [[nodiscard]] bool isWalkableForGost(Position point, Position origin) const;
+  [[nodiscard]] bool isWalkable(Position point) const;
+  [[nodiscard]] bool isInPen(Position point) const;
 
   [[nodiscard]] std::vector<PositionInt> initialPelletPositions() const;
 
@@ -33,7 +37,9 @@ public:
   static Position initialPacManPosition() { return { 13.5, 23 }; }
 
   static Position initialBlinkyPosition() { return { 13.5, 11 }; }
-  static Position blinkyScatterTarget() { return { 25, -2 }; }
+  static Position penDoorPosition() { return { 13, 11 }; }
+
+  static Position blinkyScatterTarget() { return { 25, -3 }; }
 
   static Position initialSpeedyPosition() { return { 11.5, 14 }; }
   static Position speedyScatterTarget() { return { 3, -2 }; }
--- lib/Ghost.cpp
+++ lib/Ghost.cpp
@@ -17,15 +17,86 @@ Position Ghost::position() const {
 }
 
 Position Ghost::positionInGrid() const {
-  return { std::round(pos.x), std::round(pos.y) };
+    return { std::round(pos.x), std::round(pos.y) };
 }
 
 void Ghost::update(std::chrono::milliseconds time_delta, const Board & board) {
-  time += time_delta.count();
-  if (time >= 250) {
-    time = 0;
-    alternate_animation = !alternate_animation;
-  }
+    updateAnimation(time_delta);
+    updatePosition(time_delta, board);
+}
+
+void Ghost::updatePosition(std::chrono::milliseconds time_delta, const Board & board) {
+    updateDirection(board);
+
+    float position_delta = (0.004 * time_delta.count()) * 0.75;
+
+    switch (direction) {
+      case Direction::NONE:
+        break;
+      case Direction::LEFT:
+        pos.x -= position_delta;
+        pos.y = round(pos.y);
+        break;
+      case Direction::RIGHT:
+        pos.x += position_delta;
+        pos.y = round(pos.y);
+        break;
+      case Direction::UP:
+        pos.x = round(pos.x);
+        pos.y -= position_delta;
+        break;
+      case Direction::DOWN:
+        pos.x = round(pos.x);
+        pos.y += position_delta;
+        break;
+    }
+}
+
+void Ghost::updateDirection(const Board & board) {
+    auto cell  = positionInGrid();
+    if(cell == lastIntersection)
+        return;
+
+    using P = std::tuple<Direction, Position, double>;
+
+    auto [x , y] = cell;
+    std::array<P, 4> positions = {{
+        P{Direction::UP,    {x, y-1}, 0},
+        P{Direction::LEFT,  {x-1, y}, 0},
+        P{Direction::DOWN,  {x, y+1}, 0},
+        P{Direction::RIGHT, {x+1, y}, 0}
+    }};
+    const Position target = this->target(board);
+
+    std::for_each(positions.begin(), positions.end(), [&](P & p) {
+        get<2>(p) = (get<0>(p) != oppositeDirection(direction) && board.isWalkableForGost(get<1>(p), cell)) ?
+                   std::hypot(get<1>(p).x - target.x, get<1>(p).y - target.y)
+                 : std::numeric_limits<double>::infinity();
+    });
+
+    auto it = std::min_element(positions.begin(), positions.end(), [](const auto & a, const auto &b)
+    {
+        return get<2>(a) < get<2>(b);
+    });
+
+    lastIntersection = cell;
+    direction = std::get<0>(*it);
+}
+
+Position Ghost::target(const Board & board) const {
+    if(board.isInPen(positionInGrid()))
+        return board.penDoorPosition();
+
+    return scatterTarget;
+}
+
+void Ghost::updateAnimation(std::chrono::milliseconds time_delta)
+{
+    time += time_delta.count();
+    if (time >= 250) {
+      time = 0;
+      alternate_animation = !alternate_animation;
+    }
 }
 
 Blinky::Blinky(const Board & board)
