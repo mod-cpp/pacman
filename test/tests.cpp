98c223933f4128bf4d1bdca6fea05e991c05d15c
Patricia Aas <patricia@turtlesec.no>
2021-07-07 11:40:23 +0200

    Fix test aswell

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,8 +2,7 @@
 #include <gtest/gtest.h>
 
 TEST(PacManTest, InitialPosition) {
-  pacman::Board board;
-  pacman::PacMan pacMan();
+  pacman::PacMan pacMan;
   EXPECT_EQ(pacMan.position().x, 13.5);
   EXPECT_EQ(pacMan.position().y, 23);
 }
