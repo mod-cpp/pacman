fe04e7d03acf5597d8c287f5eed62d0ebfa0d661
Corentin Jabot <corentinjabot@gmail.com>
2021-07-19 12:06:27 +0200

    Cleanup PacManAnimation a bit

--- lib/include/PacManAnimation.hpp
+++ lib/include/PacManAnimation.hpp
@@ -20,10 +20,6 @@ public:
 private:
   uint8_t animation_position = 0;
   double animation_position_delta = 0.0;
-  const GridPosition down_animation[4]{ Atlas::pacman_down_wide, Atlas::pacman_down_narrow, Atlas::pacman_closed, Atlas::pacman_down_narrow };
-  const GridPosition left_animation[4]{ Atlas::pacman_left_wide, Atlas::pacman_left_narrow, Atlas::pacman_closed, Atlas::pacman_left_narrow };
-  const GridPosition right_animation[4]{ Atlas::pacman_right_wide, Atlas::pacman_right_narrow, Atlas::pacman_closed, Atlas::pacman_right_narrow };
-  const GridPosition up_animation[4]{ Atlas::pacman_up_wide, Atlas::pacman_up_narrow, Atlas::pacman_closed, Atlas::pacman_up_narrow };
 };
 
 } // namespace pacman
