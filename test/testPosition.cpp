27517cdfa6d1123483ee9c2779a6d8a47bb70985
Ólafur Waage <olafurw@gmail.com>
2021-09-06 15:54:19 +0200

    Seperating test files from the main test cpp file.

---
+++ test/testPosition.cpp
@@ -0,0 +1,6 @@
+#include "Position.hpp"
+#include <gtest/gtest.h>
+
+TEST(PacManTest, Position) {
+  EXPECT_EQ(1, 1);
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,4 @@
-#include "PacMan.hpp"
 #include <gtest/gtest.h>
-
-TEST(PacManTest, InitialPosition) {
-  pacman::PacMan pacMan;
-  EXPECT_EQ(pacMan.position().x, 13.5);
-  EXPECT_EQ(pacMan.position().y, 23);
-}
 
 int main(int argc, char * argv[]) {
   testing::InitGoogleTest(&argc, argv);
