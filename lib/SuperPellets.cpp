b79b2a29e81b087b6609c79fbffead0a275449bc
Ólafur Waage <olafurw@gmail.com>
2021-09-08 10:21:37 +0200

    Using std::erase for pellets possible in C++20

--- lib/SuperPellets.cpp
+++ lib/SuperPellets.cpp
@@ -7,11 +7,7 @@ SuperPellets::SuperPellets()
   : positions(initialSuperPelletPositions()) {}
 
 bool SuperPellets::eatPelletAtPosition(GridPosition p) {
-  auto it = std::find(positions.begin(), positions.end(), p);
-  if (it == positions.end())
-    return false;
-  positions.erase(it);
-  return true;
+  return std::erase(positions, p) > 0;
 }
 
 } // namespace pacman
