2afe8f1d3683098c8814c3e05b85d4e51184ae1a
Ólafur Waage <olafurw@gmail.com>
2021-09-15 13:42:52 +0200

    Fixing tidy error.

--- lib/Canvas.cpp
+++ lib/Canvas.cpp
@@ -107,8 +107,8 @@ void Canvas::renderFruits(const Fruits & fruit, int eatenFruits) {
     const auto & pos = fruit.position();
     renderSprite(sprite, pos);
   }
-  const size_t x = static_cast<size_t>(LEFT_MARGIN + TARGET_MAZE_WIDTH + LEFT_MARGIN);
-  const size_t y = static_cast<size_t>((TARGET_MAZE_HEIGHT / 3.0) * 2);
+  const auto x = static_cast<size_t>(LEFT_MARGIN + TARGET_MAZE_WIDTH + LEFT_MARGIN);
+  const auto y = static_cast<size_t>((TARGET_MAZE_HEIGHT / 3.0) * 2);
 
   for (auto i = 0; i < eatenFruits + 1; i++) {
     auto sprite_position = float(i) * SPRITE_WIDTH * 1.5f;
