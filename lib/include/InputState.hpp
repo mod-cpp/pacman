be472fa4b3020001b1ffb1135a80dde1d9d5c14d
Corentin Jabot <corentinjabot@gmail.com>
2021-10-06 13:38:24 +0200

    Slide driven development

--- lib/include/InputState.hpp
+++ lib/include/InputState.hpp
@@ -12,14 +12,14 @@ public:
   bool enableAI = false;
 
   Direction direction() const {
-    if (left)
-      return Direction::LEFT;
-    if (right)
-      return Direction::RIGHT;
     if (up)
       return Direction::UP;
     if (down)
       return Direction::DOWN;
+    if (left)
+      return Direction::LEFT;
+    if (right)
+      return Direction::RIGHT;
     return Direction::NONE;
   }
 };
