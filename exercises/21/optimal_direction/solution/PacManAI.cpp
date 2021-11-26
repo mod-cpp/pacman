5b2cc130927691d494e1896abc6bec3889a01d01
Patricia Aas <patricia@turtlesec.no>
2021-11-26 13:38:57 +0100

    Fix solution for optimalDirection

--- exercises/21/optimal_direction/solution/PacManAI.cpp
+++ exercises/21/optimal_direction/solution/PacManAI.cpp
@@ -1,6 +1,6 @@
 Direction PacManAI::optimalDirection(const std::array<Move, 4> & moves) {
   double closestDistance = std::numeric_limits<double>::infinity();
-  Direction dir = Direction::NONE;
+  Direction dir = Direction::LEFT;
 
   for (const auto & move : moves) {
     if (move.distanceToTarget < closestDistance) {
