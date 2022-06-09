50164d47d47b22d25e045616c4b3b6ebbb38da41
Patricia Aas <patricia@turtlesec.no>
2022-06-09 10:29:34 +0200

    Clean up script

--- dev/ubuntu.sh
+++ dev/ubuntu.sh
@@ -3,19 +3,31 @@ set -e
 
 #  bash <(curl -s https://raw.githubusercontent.com/mod-cpp/pacman/main/dev/ubuntu.sh)
 
-sudo apt update
-sudo apt upgrade
-
 # install VSCode according to https://code.visualstudio.com/docs/setup/linux
-sudo apt-get install wget gpg
+sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
+sudo apt install -y apt-transport-https
 sudo apt update
-sudo apt install code
+sudo apt install -y code
 
-# dev
-sudo apt install git ninja-build pkg-config curl zip unzip tar cmake build-essential libx11-dev libxrandr-dev libxi-dev libudev-dev libgl1-mesa-dev clang-12 g++-10
+# PacMan deps
+sudo apt install -y \
+build-essential \
+clang-12 \
+cmake \
+g++-10
+git \
+libgl1-mesa-dev \
+libudev-dev \
+libx11-dev \
+libxi-dev \
+libxrandr-dev \
+ninja-build \
+pkg-config \
+tar \
+unzip \
+zip
