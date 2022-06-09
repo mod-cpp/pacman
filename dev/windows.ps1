928036d3a8bf6b3e7e8033040087eed56c365205
Patricia Aas <patricia@turtlesec.no>
2022-06-09 14:29:33 +0200

    Install script for Windows

---
+++ dev/windows.ps1
@@ -0,0 +1,17 @@
+# Install Visual Studio Community 2022
+winget install -e --id Microsoft.VisualStudio.2022.Community
+
+# Install or update Scoop
+$ScoopMissing = $false
+try {scoop --version} catch {$ScoopMissing = $true}
+if ($ScoopMissing) {
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+} else {
+    scoop update
+}
+
+# Add Scoop buckets
+scoop bucket add extras
+
+# Install PacMan dependencies
+scoop install vscode git ninja cmake
